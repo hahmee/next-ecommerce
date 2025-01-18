e5920e5795ed34efd5354d04bfc7fe5a1c46e7c2
HYE <hamye4143@gmail.com>
2025-01-18 12:00:41 +0900

    Fix Healthcheck

--- back/src/main/java/org/zerock/mallapi/controller/HealthController.java
+++ back/src/main/java/org/zerock/mallapi/controller/HealthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/healthcheck")
 public class HealthController {
 
-  @GetMapping("/")
+  @GetMapping()
   public String get() {
     return "OK";
   }
