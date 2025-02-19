84d06c4bcde8278b078cba5ad42ce69ab2b53f1b
HYE <hamye4143@gmail.com>
2025-02-19 22:57:17 +0900

    Change GoogleConfig

--- back/src/main/java/org/zerock/mallapi/config/GoogleConfig.java
+++ back/src/main/java/org/zerock/mallapi/config/GoogleConfig.java
@@ -1,22 +1,32 @@
 package org.zerock.mallapi.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class GoogleConfig {
-    @Value("${google.credentials.path}")
-    private String credentialsPath;
 
     @Bean
     public GoogleCredentials googleCredentials() throws IOException {
-        try (InputStream credentialsStream = new FileInputStream(credentialsPath)) {
+        // 환경 변수에서 Google Credentials 파일 경로 가져오기
+        String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+
+        if (credentialsPath == null || credentialsPath.isEmpty()) {
+            throw new IOException("환경 변수 'GOOGLE_APPLICATION_CREDENTIALS'가 설정되지 않았습니다.");
+        }
+
+        File credentialsFile = new File(credentialsPath);
+        if (!credentialsFile.exists()) {
+            throw new IOException("Google Credentials 파일을 찾을 수 없습니다: " + credentialsPath);
+        }
+
+        try (InputStream credentialsStream = new FileInputStream(credentialsFile)) {
             return GoogleCredentials.fromStream(credentialsStream);
         }
     }
