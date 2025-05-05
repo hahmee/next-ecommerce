294cf55c3fbd32603131f0f3f265ef2e7ec65f14
HYE <hamye4143@gmail.com>
2025-05-05 19:55:07 +0900

    test: 제품 등록 시나리오에 대한 E2E 테스트 추가

--- client/cypress/e2e/registerProduct.cy.ts
+++ client/cypress/e2e/registerProduct.cy.ts
@@ -2,11 +2,8 @@ describe('상품등록 E2E 테스트', () => {
     beforeEach(() => {
         // 커스텀 명령어로 로그인
         cy.login('user1@aaa.com', '1111');
-        // 🔥 여기서 모든 form submit을 방지
-        cy.window().then((win) => {
-            win.addEventListener('submit', (e) => e.preventDefault(), true);
-        });
-        cy.visit('/admin/products/add-product'); // ✅ 등록 페이지로 이동
+
+        cy.visit('/admin/products/add-product'); // 등록 페이지로 이동
 
     });
     it('정상적으로 상품을 등록할 수 있어야 한다', () => {
@@ -49,6 +46,51 @@ describe('상품등록 E2E 테스트', () => {
 
         cy.get('button[type="submit"]').should('exist').click({ force: true });
 
+        cy.contains('업로드 성공했습니다.').should('exist');
 
+        cy.url().should('include', '/admin/products');
+    });
+
+
+    it('이미지를 첨부를 안 했으면 에러 메시지가 출력되어야 한다', () => {
+
+        //카테고리 선택
+        cy.clickUntilLeaf(); // 0부터 시작해서 끝까지 자동 클릭
+
+        cy.get('[data-testid="color-input"]').as('colorInput');
+        cy.get('@colorInput').type('레드', { force: true });
+        cy.get('[data-testid="color-input"]').trigger('keydown', { key: 'Enter', force: true });
+        cy.get('@colorInput').type('노랑', { force: true });
+        cy.get('[data-testid="color-input"]').trigger('keydown', { key: 'Enter', force: true });
+        cy.contains('레드').should('exist');
+        cy.contains('노랑').should('exist');
+        // 환불/교환 정책
+        cy.get('textarea[name="refundPolicy"]', { timeout: 5000 }).should('exist').type('환불 정책 테스트', { force: true });
+        cy.get('textarea[name="changePolicy"]', { timeout: 5000 }).should('exist').type('교환 정책 테스트', { force: true });
+        cy.get('input[name="pname"]').type('테스트 상품', { force: true });
+        cy.get('input[name="price"]').type('15000', { force: true });
+        cy.get('input[name="sku"]').type('SKU123', { force: true });
+
+        cy.get('input[name="receiver"]').should('not.exist'); // 등록폼에 불필요한 필드 방지 체크
+        // 멀티 셀렉트 사이즈
+        cy.get('[data-testid="multiSizeSelect"]').click({ force: true });
+
+        cy.get('[data-testid="multiSelectValue"]').first().click({ force: true });
+        cy.get('body').type('{esc}', {force: true}); // 드롭다운 닫기
+
+
+        // 라디오 버튼 (판매중)
+        cy.get('input[type="radio"][value="ONSALE"]').check({ force: true });
+
+        cy.get('form[data-testid="product-form"]', { timeout: 10000 }).should('exist');
+
+        cy.get('.ql-editor', { timeout: 10000 })
+            .should('exist')
+            .click({ force: true })
+            .type('테스트 상세 설명입니다.', { force: true });
+
+        cy.get('button[type="submit"]').click({ force: true });
+
+        cy.contains('이미지는 한 개 이상 첨부해주세요.').should('exist');
     });
 });
--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -5,7 +5,7 @@ Cypress.Commands.add('login', (email: string, password: string) => {
     cy.get('input[name="email"]').clear().type(email);
     cy.get('input[name="password"]').clear().type(password);
     cy.get('button[type="submit"]').click();
-    cy.url().should('include', '/');  // 메인 페이지로 리디렉션되었는지 확인  // cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
+    cy.url().should('include', '/');  // 메인 페이지로 리디렉션되었는지 확인
 
     cy.wait(1000); // 쿠키가 설정되기 전에 기다리기
 
@@ -13,13 +13,8 @@ Cypress.Commands.add('login', (email: string, password: string) => {
     cy.getCookie('member', { timeout: 10000 }).should('exist').then((cookie:any) => {
         // 쿠키가 존재하면, JSON으로 파싱한 후 email이 포함되어 있는지 확인
         // URL-decoding the cookie value
-        console.log('cookie.value', cookie.value);
         const decodedCookie = decodeURIComponent(cookie.value);
-
-        // Parse the decoded cookie value to JSON
         const cookieValue = JSON.parse(decodedCookie);
-
-        // Verify the email property
         expect(cookieValue).to.have.property('email', email);
 
     });
@@ -31,7 +26,6 @@ Cypress.Commands.add('login', (email: string, password: string) => {
 Cypress.Commands.add('clickUntilLeaf', (level = 0) => {
     function clickNext(lvl: number) {
         cy.get(`[data-testid="category"][data-level="${lvl}"]`).first().then(($el) => {
-            console.log('asdfasdf', $el);
             if ($el.length > 0) {
                 cy.wrap($el).click();
                 cy.wait(300); // 필요에 따라 조정
