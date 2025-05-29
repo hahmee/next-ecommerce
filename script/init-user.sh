342f97c4427019eaaf66c204b8e852b88fdac7b5
HYE <hamye4143@gmail.com>
2025-05-29 12:35:32 +0900

    init-user.sh 수정

--- script/init-user.sh
+++ script/init-user.sh
@@ -2,7 +2,7 @@
 set -e
 
 # MariaDB 시작 후 root로 외부 접속 가능한 유저 추가
-mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" <<-EOSQL
+mariadb -u root -p"${DB_ROOT_PASSWORD}" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
     CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
     GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
