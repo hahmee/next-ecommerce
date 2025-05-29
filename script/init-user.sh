0bc25f77e71a7e27c3bd2516672602a0bc3645e0
HYE <hamye4143@gmail.com>
2025-05-29 14:33:43 +0900

    docker-compose.yml - init-user.sh 다시 추가

--- script/init-user.sh
+++ script/init-user.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-set -e
+  #!/bin/bash
+  set -e
 
-# MariaDB 시작 후 root로 외부 접속 가능한 유저 추가
-mariadb -u root -p"${DB_ROOT_PASSWORD}" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
-    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-    GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
-    FLUSH PRIVILEGES;
-EOSQL
+  # MariaDB 시작 후 root로 외부 접속 가능한 유저 추가
+  mariadb -u root -p"${DB_ROOT_PASSWORD}" <<-EOSQL
+      CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+      CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+      GRANT ALL PRIVILEGES ON `${DB_NAME}`.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+      FLUSH PRIVILEGES;
+  EOSQL
