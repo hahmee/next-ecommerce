5c457f7055761c6881b4695747524892d2a3dada
hahmee <hamye4143@gmail.com>
2025-06-27 13:18:10 +0900

    next-sitemap 설치 및 설정

---
+++ client/next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL, // 실제 배포 도메인
+  generateRobotsTxt: true,                // robots.txt도 함께 생성
+  sitemapSize: 7000,                      // 기본값 사용 가능
+  changefreq: 'daily',                    // 페이지 변경 빈도
+  priority: 0.7,                          // 우선순위
+  trailingSlash: false,                  // URL에 / 붙일지 여부
+};
