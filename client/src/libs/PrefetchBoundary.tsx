7af5fdad659e1f6c08a9a26fbd57ed2395940f78
hahmee <hamye4143@gmail.com>
2025-09-03 18:34:05 +0900

    Fix: PrefetchBoundary에async 누락 복구(await 오류 수정)

--- client/src/libs/PrefetchBoundary.tsx
+++ client/src/libs/PrefetchBoundary.tsx
@@ -15,11 +15,13 @@ type Props = {
   children: ReactNode;
 };
 
-export const PrefetchBoundary = ({ prefetchOptions, prefetchInfiniteOptions, children }: Props) => {
-  // QueryClient: 데이터 캐시 저장소
-  const queryClient = new QueryClient();
+export async function PrefetchBoundary({
+  prefetchOptions,
+  prefetchInfiniteOptions,
+  children,
+}: Props) {
+  const queryClient = new QueryClient(); //데이터 캐시 저장소
 
-  // prefetchQuery() / prefetchInfiniteQuery() -> 서버에서 데이터를 미리 요청 하고 캐시에 저장
   if (prefetchOptions) {
     Array.isArray(prefetchOptions)
       ? await Promise.all(
@@ -28,7 +30,7 @@ export const PrefetchBoundary = ({ prefetchOptions, prefetchInfiniteOptions, chi
       : await queryClient.prefetchQuery(prefetchOptions);
   }
 
-  // 무한 스크롤링일 때
+  //무한 스크롤링일 때
   if (prefetchInfiniteOptions) {
     Array.isArray(prefetchInfiniteOptions)
       ? await Promise.all(
@@ -39,7 +41,5 @@ export const PrefetchBoundary = ({ prefetchOptions, prefetchInfiniteOptions, chi
       : await queryClient.prefetchInfiniteQuery(prefetchInfiniteOptions);
   }
 
-  // dehydrate : 캐시된 데이터 직렬화 시킨다
-  // HydrationBoundary: 직렬화된 캐시를 클라이언트에 전달하여 hydrate(복원)처리
   return <HydrationBoundary state={dehydrate(queryClient)}>{children}</HydrationBoundary>;
-};
+}
